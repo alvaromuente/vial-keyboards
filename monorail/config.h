1b51fb4b43aa2c971f7a98b9b6a3f5e94147651c
Alvaro Muente <afmuente@hotmail.com>
2022-07-07 02:55:13 -0500

    rgblight to info.json

--- monorail/config.h
+++ monorail/config.h
@@ -6,9 +6,3 @@
 
 #define LED_CAPS_LOCK_PIN F4
 #define LED_PIN_ON_STATE 0
-
-#define RGB_DI_PIN B0
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
