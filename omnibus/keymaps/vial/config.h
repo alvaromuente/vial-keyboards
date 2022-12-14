83d3a163c132d5a147c33de5685470c6e0b4f787
Alvaro Muente <afmuente@outlook.com>
2022-12-13 23:38:30 -0500

    omnibus added

---
+++ omnibus/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x61, 0x6F, 0x6D, 0x6E, 0x69, 0x62, 0x75, 0x73}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
